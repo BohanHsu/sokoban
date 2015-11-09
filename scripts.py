8eb1cf34677b229339cdd8964a1e2d731ebefdb0
BohanHsu <ilovebarce@gmail.com>
2015-11-08 21:06:04 -0500

    add bfs implementation

--- scripts.py
+++ scripts.py
@@ -2,11 +2,12 @@ from solver import *
 from newgameboard import *
 from ui import *
 
-initialGameBoard = mapToBoard('./map/game1.map')
+initialGameBoard = mapToBoard('./map/game2.map')
 
 solver = Solver(initialGameBoard)
 
-result = solver.dfs()
+#result = solver.dfs()
+result = solver.bfs()
 
 boards = result['boards']
 
--- solver.py
+++ solver.py
@@ -1,6 +1,38 @@
+from collections import deque
+
 class Solver:
     def __init__(self, gameBoard):
         self.gameBoard = gameBoard
+
+    def bfs(self):
+        """
+            naive bfs
+        """
+        queue = deque([self.gameBoard])
+        parents = {self.gameBoard: None}
+
+        while len(parents) != 0:
+            gb = queue.popleft()
+            if gb.isWin():
+                path = []
+                boards = []
+                curGb = gb
+                while curGb in parents and not parents[curGb] is None:
+                    boards.insert(0, curGb)
+                    path.insert(0, parents[curGb].direction)
+                    curGb = parents[curGb].parent
+
+                boards.insert(0, curGb)
+                return {'path': path, 'boards': boards}
+
+            if gb.isGameOver():
+                continue
+
+            for direction in ['n', 's', 'e', 'w']:
+                movedGB = gb.moveRobot(direction)
+                if not movedGB is None and not movedGB in parents:
+                    queue.append(movedGB)
+                    parents[movedGB] = Parent(gb, direction)
 
     def dfs(self):
         """
